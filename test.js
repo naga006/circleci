3f980b7bb4347299dd40b59584346be07951a5b6
naga006 <91175088+naga006@users.noreply.github.com>
2023-05-19 12:18:16 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
 describe("Example Test 1", () => {
-  it("should return 'Last Day ' for GET /example", () => {
+  it("should return 'end of semester ' for GET /example", () => {
     const mockRequest = httpMocks.createRequest({
       method: "GET",
       url: "/example"
